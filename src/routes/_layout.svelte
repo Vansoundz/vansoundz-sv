c8f11ed35e23632c2bea5527352bbcc6bfc3236c
vansoundz <thebeatplanet.silente@gmail.com>
2020-11-15 11:01:33 +0300

    added nprogress

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,9 +1,26 @@
 <script lang="ts">
   import { onMount } from "svelte";
-
+  import "nprogress/nprogress.css";
   import Nav from "../components/Nav.svelte";
   import { getProjects } from "../data";
   import { store } from "../store";
+  import { stores } from "@sapper/app";
+  import NProgress from "nprogress";
+
+  const { preloading } = stores();
+
+  NProgress.configure({
+    showSpinner: false,
+  });
+
+  $: {
+    if ($preloading) {
+      NProgress.start();
+    }
+    if (!$preloading) {
+      NProgress.done();
+    }
+  }
 
   onMount(async () => {
     store.setPropjects(await getProjects());
